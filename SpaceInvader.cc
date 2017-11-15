369c5a511e72f79245ad3bbb843e9c4041aa6e56
rodriguezma <rodriguezma@users.noreply.github.com>
2017-11-15 15:38:00 +0100

    Fichero base ejemplo

---
+++ SpaceInvader.cc
@@ -0,0 +1,62 @@
+#include <esat/window.h>
+#include <esat/draw.h>
+#include <esat/sprite.h>
+#include <esat/input.h>
+#include <esat/time.h>
+
+
+#include <esat_extra/soloud/soloud.h>
+#include <esat_extra/soloud/soloud_wav.h>
+
+ SoLoud::Soloud canal;
+ //Declaración variables canal audio.
+ SoLoud::Wav ejemplo1,ejemplo2;
+
+
+int esat::main(int argc, char **argv) {
+ 
+  double current_time,last_time;
+  unsigned char fps=25;
+
+  //Inicialicización sistema audio.
+  canal.init();
+  //Carga audio en cada variable canal audio.
+  ejemplo1.load("./Recursos/Audio/ogg/dp_frogger_extra.ogg");
+  ejemplo2.load("./Recursos/Audio/ogg/dp_frogger_start.ogg");
+ 
+  esat::WindowInit(800,600);
+  WindowSetMouseVisibility(true);
+  
+
+  while(esat::WindowIsOpened() && !esat::IsSpecialKeyDown(esat::kSpecialKey_Escape)) {
+	last_time = esat::Time();
+	
+	esat::DrawBegin();
+    esat::DrawClear(0,0,0);
+   
+    if(esat::IsSpecialKeyPressed(esat::kSpecialKey_Left)){
+      canal.play(ejemplo1); //Inicia reproducción canal
+    }
+    if(esat::IsSpecialKeyPressed(esat::kSpecialKey_Right)){
+      ejemplo1.stop(); //Finaliza reproducción canal
+    }
+    if(esat::IsSpecialKeyPressed(esat::kSpecialKey_Up)){
+      canal.play(ejemplo2);
+    }
+    if(esat::IsSpecialKeyPressed(esat::kSpecialKey_Down)){
+      ejemplo2.stop();
+    }
+    
+    esat::DrawEnd();
+	
+	do{
+    current_time = esat::Time();
+    }while((current_time-last_time)<=1000.0/fps);
+    esat::WindowFrame();
+  }
+  
+  esat::WindowDestroy();
+   //Finalización sitema audio.
+  canal.deinit();
+  return 0;
+}
