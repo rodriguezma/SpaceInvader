017376f2777f842aa342ca0dd41a9b7d7b58cdcb
rodriguezma <33692368+rodriguezma@users.noreply.github.com>
2017-11-16 12:38:32 +0100

    Col
    This reverts commit 4532e5e0f4f8f59a9526ddd7374b8f024fb519e5, reversing
    changes made to d29923f83d10b32b737bef4af759567236024a3d.

--- SpaceInvader.cc
+++ SpaceInvader.cc
@@ -8,7 +8,6 @@
 #include <esat_extra/soloud/soloud_wav.h>
 
 #include <stdio.h>
-#include <stdlib.h>
 
  SoLoud::Soloud canal;
  //Declaración variables canal audio.
@@ -16,8 +15,16 @@
  
  esat::SpriteHandle hoja;
  esat::SpriteHandle invA[2], invB[2], invC[2];
- int points = 0;
- char pointsSTR[25];
+
+ int Wwhidth=800;
+ int Wheight=600;
+
+ int auxMov=1;
+ int auxM=4;
+ int temp=0;
+ float pointsSTR=0;
+ char puntuacion[10];
+
 //////ESTRUCTURAS//////
  
  struct Player{
@@ -31,10 +38,10 @@
   struct marcianitos{
 	
   int x, y;
-  int tipo;
+  int tipo, anim=0;
   esat::SpriteHandle sprite;
 	
-}enemies[50];
+  }enemies[50];
 
 
 
@@ -81,6 +88,18 @@ struct tDisparo{			//Estructura de los disparos
 
 
 //////FUNCIONES//////
+
+bool Col(int a1x, int a1y, int a2x, int a2y, int b1x, int b1y, int b2x, int b2y){
+
+	if (b2x < a1x || b1x > a2x)
+		return false;
+	else if (b2y < a1y ||  b1y > a2y)
+		return false;
+	else
+		return true;
+
+}
+
   void CargarEnemigos(){
   
   invA[0]= esat::SubSprite(hoja, 0, 85, 24, 16);
@@ -98,22 +117,53 @@ void CargaSprites(){
     player1.sprite=esat::SubSprite(hoja, 48, 64, 26, 16);
 
     CargarEnemigos();
-
-      for (int i=0; i<50; ++i){
-    if (i< 10){
-      enemies[i].sprite = invC[0];
-    
-    }else if (i>=10 && i<30){
-      enemies[i].sprite = invB[0];
-      
-    }else if (i>=30 && i<50){
-      enemies[i].sprite = invA[0];
+	
+		  
     }
-  } 
+   
 
+
+	void InitAnimacion(){
+		for (int i=0; i<50; ++i){
+			if(i%2==0)
+				enemies[i].anim = 1;
+			
+		}	
 }
 
+	void CambiarAnimacion(){
+		
+		for (int i=0; i<50; ++i){
+	if (i< 10){
+      enemies[i].sprite = invC[enemies[i].anim];
+    
+    }else if (i>=10 && i<30){
+      enemies[i].sprite = invB[enemies[i].anim];
+      
+    }else if (i>=30 && i<50){
+      enemies[i].sprite = invA[enemies[i].anim];
+	}
+		if(enemies[i].anim==0 && temp==5){
+			++enemies[i].anim;
+		}else if(temp==5)
+			--enemies[i].anim;
+		
+		}
+	}
+	
+	void Temporizacion(){
+		
+		++temp;
+		if(temp>5)
+		temp=0;
+		
+	}
+	
+
 void InitMarcianos(){
+
+  int a=Wwhidth/4;
+
   for(int i=0;i<5;i++){
    for(int j=0;j<10;j++){
      int t;
@@ -125,11 +175,56 @@ void InitMarcianos(){
       t=1;
 
       enemies[i*10+j].tipo=t;
+
       enemies[i*10+j].x=200+j*30;
       enemies[i*10+j].y=100+i*25;
 
     }
   } 
+}
+
+void MovMarcianos(){
+  
+  switch(auxMov){
+    case 1:
+      for(int i=0;i<10;i++){
+        enemies[auxM*10+i].x+=4;
+      }
+      if(auxM==0)
+        auxM=4;
+      else
+        auxM--;
+      break;
+    case 2:
+     for(int i=0;i<10;i++){
+        enemies[auxM*10+i].x-=4;
+      }
+      if(auxM==0)
+        auxM=4;
+      else
+        auxM--;
+      break;
+    }
+
+}
+
+void BordeMarcianos(){
+  int a=Wwhidth/4;
+  int b=a*3;
+  
+  if(enemies[0].x < a){
+    for(int i=0;i<50;i++){
+        enemies[i].y+=4;
+      }
+    auxMov=1;
+  }
+  else if(enemies[9].x+esat::SpriteWidth(enemies[9].sprite)>b){
+    for(int i=0;i<50;i++){
+        enemies[i].y+=4;
+      }
+    auxMov=2;
+  } 
+
 }
 
 
@@ -141,18 +236,19 @@ void InitMarcianos(){
 
     }else if (esat::IsSpecialKeyPressed(esat::kSpecialKey_Right) && player1.x + esat::SpriteWidth(player1.sprite) < 600){
       player1.x+=6;
-
     }
 
   }
+  
   
   
 
 
   void UpdateFrame(){
 	
+	
 	esat::DrawText(100,50,"SCORE:");
-	esat::DrawText(175,50,pointsSTR);
+	esat::DrawText(175,50,itoa(pointsSTR,puntuacion,10));
 	esat::DrawText(580,50,"LIVES:");
 	
     esat::DrawSprite(player1.sprite,player1.x,player1.y);
@@ -177,28 +273,29 @@ int esat::main(int argc, char **argv) {
   //ejemplo1.load("./Recursos/Audio/ogg/dp_frogger_extra.ogg");
   //ejemplo2.load("./Recursos/Audio/ogg/dp_frogger_start.ogg");
  
-  esat::WindowInit(800,600);
+  esat::WindowInit(Wwhidth,Wheight);
   WindowSetMouseVisibility(true);
 
   CargaSprites();
   InitMarcianos();
-  
+  InitAnimacion();
   esat::DrawSetTextFont("./Recursos/Fuentes/space_invaders.ttf");
-  esat::DrawSetTextSize(25.0f);
-  esat::DrawSetFillColor(255,255,255);
+  esat::DrawSetFillColor(255,255,255,255);
 
   while(esat::WindowIsOpened() && !esat::IsSpecialKeyDown(esat::kSpecialKey_Escape)) {
     
     last_time = esat::Time();
-		itoa(points,pointsSTR,10);
     esat::DrawBegin();
     esat::DrawClear(0,0,0);
-	
-
+   
+	CambiarAnimacion();
     MovPlayer();
-  
-	UpdateFrame();
 
+	MovMarcianos();
+    BordeMarcianos();
+    UpdateFrame();
+	
+	Temporizacion();
 
 
 
